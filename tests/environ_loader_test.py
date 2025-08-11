b087c55d755d178a1e79cc42e0f970e97b26a45b
Preocts <preocts@preocts.com>
2025-08-10 22:43:17 -0400

    remove destructive fixture in favor of conftest

--- tests/environ_loader_test.py
+++ tests/environ_loader_test.py
@@ -2,23 +2,12 @@ from __future__ import annotations
 
 import os
 
-import pytest
-
 from eggviron._environ_loader import EnvironLoader
-
-
-@pytest.fixture(autouse=True)
-def clear_environ() -> None:
-    # Clearing the os.environ before tests will help prevent leaking any actual secrets
-    # in the environment during a failure in the test run-time.
-    for key in os.environ.keys():
-        os.environ.pop(key)
-
-    os.environ["SOME_FAKE_KEY"] = "foo"
 
 
 def test_environ_loader_returns_os_environ() -> None:
     # This is a simple loader doing a simple task
+    os.environ["SOME_FAKE_KEY"] = "foo"
     expected_results = dict(os.environ)
 
     results = EnvironLoader().run()
