316291ca3eb89a8acad004cd52825c47a576a324
Preocts <preocts@preocts.com>
2025-08-16 01:29:08 -0400

    Replace moto with vcrpy (#32)
    * Disable env wipe while recording
    
    * Remove moto
    
    * Remove moto, add vcrpy
    
    * Adjust clear_environ to mimic creds for recorded tests
    
    * Add vcrpy for recording AWS calls
    
    * add recorded cassettes
    
    * .env.sample for recording of tests

--- tests/awsparamstore_loader_test.py
+++ tests/awsparamstore_loader_test.py
@@ -1,31 +1,31 @@
 from __future__ import annotations
 
-from collections.abc import Generator
+import os
 from unittest.mock import patch
 
 import pytest
-from moto import mock_aws
+import vcr  # type: ignore
 
 from eggviron import AWSParamStore
 from eggviron import AWSParamStoreException
 
 boto3 = pytest.importorskip("boto3")
 
-DEFAULT_REGION = "us-east-2"
-
-
-@pytest.fixture
-def mock_ssm() -> Generator[None, None, None]:
-    """Mock the parameter store."""
-    with mock_aws():
-        client = boto3.client("ssm", DEFAULT_REGION)
-        client.put_parameter(Name="/biz/baz", Value="biz.baz", Type="String")
-        client.put_parameter(Name="/foo/bar", Value="foo.bar", Type="String")
-        client.put_parameter(Name="/foo/baz", Value="foo.baz", Type="SecureString")
-        client.put_parameter(Name="/foo/biz", Value="foo,biz", Type="StringList")
-        client.put_parameter(Name="/foo/foo2/bar", Value="foo foo bar", Type="String")
-
-        yield None
+recorder = vcr.VCR(
+    cassette_library_dir="tests/cassettes",
+    record_mode="once",
+    filter_headers=[
+        "Authorization",
+        "User-Agent",
+        "X-Amz-Date",
+        "X-Amz-Target",
+    ],
+    match_on=[
+        "method",
+        "body",
+        "url",
+    ],
+)
 
 
 def test_init_with_boto3() -> None:
@@ -68,47 +68,47 @@ def test_init_without_path_or_name_raises() -> None:
 
 def test_run_raises_without_region() -> None:
     """When the region is not defined an exception is raised."""
+    os.environ.pop("AWS_DEFAULT_REGION", None)
 
     with pytest.raises(AWSParamStoreException):
         AWSParamStore(parameter_name="/foo/bar").run()
 
 
-@pytest.mark.usefixtures("mock_ssm")
+def test_run_raises_exception_when_name_not_found() -> None:
+    """Ask for a parameter that does not exist to raise an exception"""
+
+    with pytest.raises(AWSParamStoreException):
+        AWSParamStore(parameter_name="/oo/bar").run()
+
+
+@recorder.use_cassette()
 def test_run_returns_parameter_by_name_without_truncation() -> None:
     """Return single value with full path as the key"""
 
-    result = AWSParamStore(parameter_name="/foo/bar", aws_region=DEFAULT_REGION).run()
+    result = AWSParamStore(parameter_name="/foo/bar").run()
 
     assert result == {"/foo/bar": "foo.bar"}
 
 
-@pytest.mark.usefixtures("mock_ssm")
+@recorder.use_cassette()
 def test_run_returns_parameter_by_name_with_truncation() -> None:
     """Return single value with just the final component of the path as the key"""
-    clazz = AWSParamStore(parameter_name="/foo/bar", aws_region=DEFAULT_REGION, truncate_key=True)
+    clazz = AWSParamStore(parameter_name="/foo/bar", truncate_key=True)
 
     result = clazz.run()
 
     assert result == {"bar": "foo.bar"}
 
 
-@pytest.mark.usefixtures("mock_ssm")
-def test_run_raises_exception_when_name_not_found() -> None:
-    """Ask for a parameter that does not exist to raise an exception"""
-
-    with pytest.raises(AWSParamStoreException):
-        AWSParamStore(parameter_name="/oo/bar", aws_region=DEFAULT_REGION).run()
-
-
-@pytest.mark.usefixtures("mock_ssm")
+@recorder.use_cassette()
 def test_run_returns_parameters_by_path_without_truncation() -> None:
     """Return all paramters found in the path, nonrecursively, with pagination"""
     expected = {
         "/foo/bar": "foo.bar",
-        "/foo/baz": "kms:alias/aws/ssm:foo.baz",
+        "/foo/baz": "AQICAHgabVcv70rp9mLGkzhrqCN34++39E3yG6opT3oWiUeIjQHzlmsgV5Gcanz46b8VrQzAAAAAZTBjBgkqhkiG9w0BBwagVjBUAgEAME8GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMUdY1ipv5IuX83HMWAgEQgCLdD70T7esiOPM+anDBsrleqSXCQJJFB0sDOSeAd9wpVqwT",
         "/foo/biz": "foo,biz",
     }
-    clazz = AWSParamStore(parameter_path="/foo/", aws_region=DEFAULT_REGION)
+    clazz = AWSParamStore(parameter_path="/foo/")
 
     with patch("eggviron._awsparamstore_loader._MAX_RESULTS", 1):
         results = clazz.run()
@@ -116,16 +116,16 @@ def test_run_returns_parameters_by_path_without_truncation() -> None:
     assert results == expected
 
 
-@pytest.mark.usefixtures("mock_ssm")
+@recorder.use_cassette()
 def test_run_returns_parameters_by_path_without_truncation_recursively() -> None:
     """Return all paramters found in the path, recursively, with pagination"""
     expected = {
         "/foo/bar": "foo.bar",
-        "/foo/baz": "kms:alias/aws/ssm:foo.baz",
+        "/foo/baz": "AQICAHgabVcv70rp9mLGkzhrqCN34++39E3yG6opT3oWiUeIjQHzlmsgV5Gcanz46b8VrQzAAAAAZTBjBgkqhkiG9w0BBwagVjBUAgEAME8GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMUdY1ipv5IuX83HMWAgEQgCLdD70T7esiOPM+anDBsrleqSXCQJJFB0sDOSeAd9wpVqwT",
         "/foo/biz": "foo,biz",
         "/foo/foo2/bar": "foo foo bar",
     }
-    clazz = AWSParamStore(parameter_path="/foo/", aws_region=DEFAULT_REGION, recursive=True)
+    clazz = AWSParamStore(parameter_path="/foo/", recursive=True)
 
     with patch("eggviron._awsparamstore_loader._MAX_RESULTS", 1):
         results = clazz.run()
@@ -133,15 +133,15 @@ def test_run_returns_parameters_by_path_without_truncation_recursively() -> None
     assert results == expected
 
 
-@pytest.mark.usefixtures("mock_ssm")
+@recorder.use_cassette()
 def test_run_returns_parameters_by_path_within_truncation() -> None:
     """Return all paramters found in the path, nonrecursively, with pagination"""
     expected = {
         "bar": "foo.bar",
-        "baz": "kms:alias/aws/ssm:foo.baz",
+        "baz": "AQICAHgabVcv70rp9mLGkzhrqCN34++39E3yG6opT3oWiUeIjQHzlmsgV5Gcanz46b8VrQzAAAAAZTBjBgkqhkiG9w0BBwagVjBUAgEAME8GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMUdY1ipv5IuX83HMWAgEQgCLdD70T7esiOPM+anDBsrleqSXCQJJFB0sDOSeAd9wpVqwT",
         "biz": "foo,biz",
     }
-    clazz = AWSParamStore(parameter_path="/foo/", aws_region=DEFAULT_REGION, truncate_key=True)
+    clazz = AWSParamStore(parameter_path="/foo/", truncate_key=True)
 
     with patch("eggviron._awsparamstore_loader._MAX_RESULTS", 1):
         results = clazz.run()
@@ -149,15 +149,13 @@ def test_run_returns_parameters_by_path_within_truncation() -> None:
     assert results == expected
 
 
-@pytest.mark.usefixtures("mock_ssm")
 def test_run_returns_parameters_by_path_raises_when_exceeds_max_pagination() -> None:
     """A safe-guard against infinite loops, raise if max pagination attempts are reached."""
-    pattern = "Max pagination loop exceeded: _MAX_PAGINATION_LOOPS=1"
-    clazz = AWSParamStore(parameter_path="/foo/", aws_region=DEFAULT_REGION)
+    pattern = "Max pagination loop exceeded: _MAX_PAGINATION_LOOPS=0"
+    clazz = AWSParamStore(parameter_path="/foo/")
 
     with (
-        patch("eggviron._awsparamstore_loader._MAX_RESULTS", 1),
-        patch("eggviron._awsparamstore_loader._MAX_PAGINATION_LOOPS", 1),
+        patch("eggviron._awsparamstore_loader._MAX_PAGINATION_LOOPS", 0),
         pytest.raises(AWSParamStoreException, match=pattern),
     ):
         clazz.run()
--- tests/conftest.py
+++ tests/conftest.py
@@ -10,5 +10,11 @@ import pytest
 @pytest.fixture(autouse=True)
 def clear_environ() -> Generator[None, None, None]:
     """Ensure the environment variables are clean for all test runs."""
-    with patch.dict(os.environ, {}, clear=True):
+    mocked_env = {
+        "AWS_ACCESS_KEY_ID": "mock",
+        "AWS_SECRET_ACCESS_KEY": "mock",
+        "AWS_DEFAULT_REGION": "us-east-2",
+    }
+    clear = not bool(os.getenv("ALLOW_TEST_RECORDING"))
+    with patch.dict(os.environ, mocked_env if clear else {}, clear=clear):
         yield None
